9d437557c30abed15aa176cfaede89854ca3f92e
rasmussig <rasmus2001@live.dk>
2024-12-12 10:36:05 +0100

    Update secret values in vault_init.sh for improved security

--- Vault-init/vault_init.sh
+++ Vault-init/vault_init.sh
@@ -5,7 +5,7 @@ export VAULT_TOKEN='00000000-0000-0000-0000-000000000000'
 
 sleep 10
 
-vault kv put -mount=secret secrets SecretKey=5Jw9yT4fb9T5XrwKUz23QzA5D9BuY3p6 IssuerKey=gAdDxQDQq7UYNxF3F8pLjVmGuU5u8g3y MongoConnectionString=mongodb://admin:1234@mongodb:27017
+vault kv put -mount=secret secrets SecretKey=7Kx2mP8qv6WzR9tL0jH3nB5sY1D4gF7c IssuerKey=GO_AuctionHouse MongoConnectionString=mongodb://admin:1234@mongodb:27017
 
 while :
 do
